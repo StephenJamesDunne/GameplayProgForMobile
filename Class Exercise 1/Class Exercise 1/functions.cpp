948fd678988c09cd702e476171b208309b6490d2
Stephen Dunne <C00194821@setu.ie>
2024-09-22 21:52:44 +0100

    completed up to q16 in lab 1

--- Class Exercise 1/Class Exercise 1/functions.cpp
+++ Class Exercise 1/Class Exercise 1/functions.cpp
@@ -180,6 +180,173 @@ int main()
 		std::cout << "Number was not found. " << std::endl;
 	}
 
+
+	// 9.	Find and return the second largest element in an array of positive integer, returns -1 if it can’t find one. 
+	//int find2ndLargest(int size, int arr[])
+	//{
+	//	return -1;
+	//}
+
+	std::cout << "-----------------------" << std::endl;
+	std::cout << "Q9: Find 2nd largest element in an array\n" << std::endl;
+
+
+	int arr2[] = { 5, 7, 1, 3, 7, 8, 9, 5 };
+	int size2 = 8;
+	int secondLargest = find2ndLargest(size2, arr2);
+
+	std::cout << "Second largest element in the array: " << secondLargest << std::endl;
+
+
+	std::cout << "-----------------------" << std::endl;
+	std::cout << "Q10: Copy array 1 to array 2\n" << std::endl;
+
+	const int copySize = 4;
+	int copyarr1[copySize] = { 1, 2, 3, 4 }; // defined size and values
+	int copyarr2[copySize]{};				  // defined size, no pretermined values
+
+	copyArraytoArray(copySize, copyarr1, copyarr2);
+
+	std::cout << "Elements copied to array2: " << std::endl;
+	for (int i = 0; i < copySize; i++)
+	{
+		std::cout << copyarr2[i] << " ";
+	}
+	std::cout << std::endl;
+
+	std::cout << "-----------------------" << std::endl;
+	std::cout << "Q11: Insert an element into an array at a specified position\n" << std::endl;
+
+	const int arraySize = 15;
+	int elevenArray[arraySize] = { 1, 2, 3, 4, 5 };
+	int elevenCount = 5;
+	int elementToInsert = 67;
+	int insertIndex = 2;
+
+	bool inserted = insertElement(arraySize, elevenCount, elevenArray, elementToInsert, insertIndex);
+
+	if (inserted)
+	{
+		elevenCount++; // elements in the array increased by 1
+		std::cout << elementToInsert << " inserted at position " << insertIndex << ". New arrangement of array: \n";
+		for (int i = 0; i < elevenCount; i++)
+		{
+			std::cout << elevenArray[i] << " ";
+		}
+	}
+	else
+	{
+		std::cout << "Array is full; element could not be inserted. ";
+	}
+	std::cout << std::endl;
+
+	std::cout << "-----------------------" << std::endl;
+	std::cout << "Q12: Delete an element from an array\n" << std::endl;
+ 
+	int twelveArr[] = { 1, 2, 3, 4, 5 };
+	int twelveSize = sizeof(twelveArr) / sizeof(twelveArr[0]);
+	int twelveDeleteIndex = 2;
+
+
+	std::cout << "Elements in array before deletion: ";
+	for (int i = 0; i < twelveSize; i++)
+	{
+		std::cout << twelveArr[i] << " ";
+	}
+	std::cout << std::endl;
+
+
+	bool deleted = deleteElement(twelveSize, twelveArr, twelveDeleteIndex);
+
+	if (deleted)
+	{
+		std::cout << "Elements in array after deletion: ";
+		for (int i = 0; i < twelveSize; i++)
+		{
+			std::cout << twelveArr[i] << " ";
+		}
+		std::cout << std::endl;
+	}
+	else
+	{
+		std::cout << "Invalid index, cannot delete element. \n";
+	}
+
+	std::cout << "-----------------------" << std::endl;
+	std::cout << "Q13: Count the frequency of en element in an array \n " << std::endl;
+
+	int thirteenArray[] = { 1, 2, 2, 4, 2, 5, 2 };
+	int thirteenSize = sizeof(thirteenArray) / sizeof(thirteenArray[0]);
+	int thirteenValue = 2;
+
+	std::cout << "Elements in array: ";
+	for (int i = 0; i < thirteenSize; i++)
+	{
+		std::cout << thirteenArray[i] << " ";
+	}
+	std::cout << std::endl;
+
+	int thirteenCount = frequencyCount(thirteenSize, thirteenArray, thirteenValue);
+
+	std::cout << "Value " << thirteenValue << " found in array " << thirteenCount << " times. " << std::endl;
+
+
+	std::cout << "-----------------------" << std::endl;
+	std::cout << "Q14: Count duplicates in an array \n" << std::endl;
+
+	int fourteenArr[] = { 4, 2, 4, 5, 6, 2, 7, 8, 8, 7, 2, 2, 5 };
+	int fourteenSize = sizeof(fourteenArr) / sizeof(fourteenArr[0]);
+
+	for (int i = 0; i < fourteenSize; i++)
+	{
+		std::cout << fourteenArr[i] << " ";
+	}
+	std::cout << std::endl;
+
+	int dupes = countDuplicates(fourteenSize, fourteenArr);
+
+
+	std::cout << "-----------------------" << std::endl;
+	std::cout << "Q15: Find reverse of an array  \n" << std::endl;
+
+	int fifteenArr[] = {7, 6, 5, 4, 3, 2, 1 };
+	int fifteenSize = sizeof(fifteenArr) / sizeof(fifteenArr[0]);
+
+	std::cout << "Original array: ";
+	for (int i = 0; i < fifteenSize; i++)
+	{
+		std::cout << fifteenArr[i] << " ";
+	}
+	std::cout << std::endl;
+	reverse(fifteenSize, fifteenArr);
+	std::cout << "Reversed array: ";
+	for (int i = 0; i < fifteenSize; i++)
+	{
+		std::cout << fifteenArr[i] << " ";
+	}
+	std::cout << std::endl;
 	
 
+
+	std::cout << "-----------------------" << std::endl;
+	std::cout << "Q16: Left rotate an array by one position \n" << std::endl;
+
+	int sixteenArr[] = { 6, 1, 2, 3, 4, 5 };
+	int sixteenSize = sizeof(sixteenArr) / sizeof(sixteenArr[0]);
+
+	std::cout << "Original array: ";
+	for (int i = 0; i < sixteenSize; i++)
+	{
+		std::cout << sixteenArr[i] << " ";
+	}
+	std::cout << std::endl;
+	rotateLeft(sixteenSize, sixteenArr);
+	std::cout << "Rotated array: ";
+	for (int i = 0; i < sixteenSize; i++)
+	{
+		std::cout << sixteenArr[i] << " ";
+	}
+	std::cout << std::endl;
+
+	std::cin.get();
 }
--- Class Exercise 1/Class Exercise 1/functions.h
+++ Class Exercise 1/Class Exercise 1/functions.h
@@ -164,39 +164,162 @@ int find(int toFind, int arr[], int size)
 
 int find2ndLargest(int size, int arr[])
 {
-	return -1;
+	int largest = -1;
+	int secondLargest = -1;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] > largest) // if current iteration is greater than the largest number that's currently stored
+		{
+			secondLargest = largest; // largest number is now the second largest number
+			largest = arr[i]; // store current iteration in largest variable
+		}
+		else if (arr[i] < largest && arr[i] > secondLargest) // if current iteration is bigger than secondLargest but still smaller than largest
+		{
+			secondLargest = arr[i]; // current iteration is then secondLargest number
+		}
+	}
+
+	return secondLargest; // this will return -1 if no largest/2nd largest elements are found
 }
 void copyArraytoArray(int size, int arr1[], int arr2[])
 {
+	for (int i = 0; i < size; i++)
+	{
+		arr2[i] = arr1[i];
+	}
 	return;
 }
-bool insertElement(int& size, int& count, int arr[], int elementToInsert, int insertIndex)
+bool insertElement(int size, int& count, int arr[], int elementToInsert, int insertIndex)
 {
+	if (count >= size)
+	{
+		return false;
+	}
+	else
+	{
+		for (int i = count - 1; i >= insertIndex; i--) // start at end of array and count down to insertIndex
+		{
+			arr[i + 1] = arr[i]; // shift the values to the right in the array up to the insertIndex
+		}
+
+		arr[insertIndex] = elementToInsert; // value at insertIndex is now the new element inserted
+		return true;
+	}
+
+
+
 
 	return false;
 }
-bool deleteElement(int& size, int& count, int arr[], int deleteIndex)
+
+bool deleteElement(int &size, int arr[], int deleteIndex)
 {
 
-	return true;
+
+	if (deleteIndex >= 0 && deleteIndex < size)
+	{
+		for (int i = deleteIndex; i < size; i++)
+		{
+			arr[i] = arr[i + 1];
+		}
+		size--;
+		return true;
+	}
+
+	return false;
 }
+
 int frequencyCount(int size, int arr[], int value)
 {
+	int count = 0;
 
-	return 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] == value)
+		{
+			count++;
+		}
+	}
+
+	return count;
 }
 int countDuplicates(int size, int arr[])
 {
+	bool alreadyCounted;
+	int count;					// tracks amount of dupes of current element
+	int totalDuplicates = 0;	// tracks total amount of dupes
 
-	return 0;
+	for (int i = 0; i < size; i++)
+	{
+		count = 1;				// reset the current count of dupes to 1
+		alreadyCounted = false; // reset alreadyCounted to false at the top of every iteration
+
+		// check if this element has already been counted as a duplicate
+		for (int k = 0; k < i; k++)
+		{
+			if (arr[i] == arr[k])	
+			{
+				alreadyCounted = true;
+				break;
+			}
+		}
+
+		// begin a new iteration of the i loop if the current element has already been counted
+		if (alreadyCounted)
+		{
+			continue;			
+		}
+
+		for (int j = i + 1; j < size; j++) // count occurrences of arr[i] in the rest of the array
+		{
+			if (arr[i] == arr[j])
+			{
+				count++; 
+			}
+		}
+
+		if (count > 1)
+		{
+			std::cout << arr[i] << " appears " << count << " times" << std::endl;
+			totalDuplicates += count - 1; // dupes = total times element appears - 1; if count > 1
+		}
+	}
+
+	return totalDuplicates;
 }
 void reverse(int size, int arr[])
 {
-	return;
+	int start = 0;
+	int end = size - 1;
+	int tempNum = 0;
+
+	//for (int start = 0; start < end; start++)
+	//{
+	//	tempNum = start;
+	//	arr[start] = arr[end];
+	//	arr[end] = tempNum;
+	//	end--;
+	//}
+
+	while (start < end)
+	{
+		tempNum = arr[start];
+		arr[start] = arr[end];
+		arr[end] = tempNum;
+		start++;
+		end--;
+	}
 }
-int rotateLeft(int size, int arr[])
+void rotateLeft(int size, int arr[])
 {
-	return -1;
+	int tempNum = arr[0];
+
+	for (int i = 0; i < size - 1; i++) // size - 1 so the last position is temporarily there twice, and then replaced with arr[0]
+	{
+		arr[i] = arr[i + 1]; // assign each element 1 position to the left of itself
+	}
+	arr[size - 1] = tempNum; // value from start of initial array is assigned to the rightmost element
 }
 bool twoMovies(int flightLength, int movieLengths[], int size)
 {

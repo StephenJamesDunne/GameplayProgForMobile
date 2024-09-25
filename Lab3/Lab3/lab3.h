61b6db90f9bd6e5b93a348afe9b26bdbe5237ebd
Stephen Dunne <C00194821@setu.ie>
2024-09-25 13:55:49 +0100

    part A started

--- Lab3/Lab3/lab3.h
+++ Lab3/Lab3/lab3.h
@@ -7,12 +7,16 @@
 
 void FindAllVariables(char variables[1000], char code[1000])
 {
-	int count = 0;
 	int codeArraySize = sizeof(code) / sizeof(code[0]);
+	int varArrayIterator = 0;
 
-	for (int i = 0; i < codeArraySize; i++)
+	for (int codeIndex = 0; codeIndex < codeArraySize; codeIndex++)
 	{
-		
+		if (code[codeIndex] == ' ' && code[codeIndex - 3] == 'i' && code[codeIndex - 2] == 'n' && code[codeIndex - 1 == 't'])
+			// if current element is a space, and the previous 3 elements are 'int'
+		{
+			
+		}
 	}
 }
 void FindAllVariablesInScope(char variables[1000], char code[1000], int lineNumber)

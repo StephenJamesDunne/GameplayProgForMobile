e41ba27e43ecd57aaa5ee287fc870e6afe3c2178
Stephen Dunne <C00194821@setu.ie>
2024-09-30 22:08:12 +0100

    updated lab 3 code

--- Lab3/Lab3/lab3.h
+++ Lab3/Lab3/lab3.h
@@ -7,16 +7,30 @@
 
 void FindAllVariables(char variables[1000], char code[1000])
 {
-	int codeArraySize = sizeof(code) / sizeof(code[0]);
+	int codeArraySize = 1000;
 	int varArrayIterator = 0;
 
-	for (int codeIndex = 0; codeIndex < codeArraySize; codeIndex++)
+	for (int codeIndex = varArrayIterator; codeIndex < codeArraySize; codeIndex++)
 	{
-		if (code[codeIndex] == ' ' && code[codeIndex - 3] == 'i' && code[codeIndex - 2] == 'n' && code[codeIndex - 1 == 't'])
-			// if current element is a space, and the previous 3 elements are 'int'
+		if (code[codeIndex] == '=') // found a variable declaration
 		{
-			
+			for (int variableIndex = codeIndex; variableIndex > 0; variableIndex--) // start from '='
+			{
+				if (code[variableIndex] == 't' && code[variableIndex - 1] == 'n' && code[variableIndex - 2] == 'i' && code[variableIndex + 6] != '(') // find 'int' but not 'int main'
+				{
+					for (int variableNameIndex = variableIndex + 2; code[variableNameIndex] != ' '; variableNameIndex++) // start from variable name, end at next space
+					{
+						variables[varArrayIterator] = code[variableNameIndex]; // assign each character in variable name to the new array, starting at element[0]
+						varArrayIterator++; // increment the iterator
+					}
+				}
+			}
 		}
+	}
+
+	for (int i = 0; i < codeArraySize; i++)
+	{
+		std::cout << variables[i];
 	}
 }
 void FindAllVariablesInScope(char variables[1000], char code[1000], int lineNumber)

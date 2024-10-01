a16828383c20b16b982da4484c1bd7a109163d54
Stephen Dunne <C00194821@setu.ie>
2024-10-01 18:34:41 +0100

    lab 3 part 1 done

--- Lab3/Lab3/lab3.cpp
+++ Lab3/Lab3/lab3.cpp
@@ -1,4 +1,4 @@
-//Stephen Dunne 
+// Stephen Dunne 
 // C00194821
 
 
@@ -8,17 +8,33 @@
 int main()
 {
 
-	char code[1000] = "int main() \n { \n int num1 = 5;\n int num2 = 7;\n if (num1 < 10) \n  {\n int num3 = 4;\n} \n} \n";
-
+	/*char code[1000] = "int main() \n { \n int num1 = 5;\n int num2 = 7;\n char CharacterCharIsACharacterTest = 'a';\n if (num1 < 10) \n  {\n int num3 = 4;\n} \n} \n";
 	char variables[1000] = "";
-
-	FindAllVariables(variables, code);
-
+	FindAllVariables(variables, code);*/
 
 
-
-
+	char code[1000] = "int main()\n{\n int numElephants = 10; \n int var2 = 10; \n	if (var2 < 10)\n{\nint var3 = 20; \n}\n while (var2 < 0)\n {\n if (var2 == 10) \n {\n int var4; \n} \n	int num = 10; \n} \n int var5 = 10; \n}";
+	char variables[1000] = "";
 	FindAllVariablesInScope(variables, code, 12);
 
-
+	std::cin.get();
 }
+
+//int main()
+//{
+//	int numElephants = 10;
+//	int var2 = 10;
+//	if (var2 < 10)
+//	{
+//		int var3 = 20;
+//	}
+//	while (var2 < 0)
+//	{
+//		if (var2 == 10)
+//		{
+//			int var4;
+//		}
+//		int num = 10;
+//	}
+//	int var5 = 10;
+//}
--- Lab3/Lab3/lab3.h
+++ Lab3/Lab3/lab3.h
@@ -8,30 +8,50 @@
 void FindAllVariables(char variables[1000], char code[1000])
 {
 	int codeArraySize = 1000;
-	int varArrayIterator = 0;
+	int varArrayIterator = 0; // track which element of the variables array is currently being iterated
 
-	for (int codeIndex = varArrayIterator; codeIndex < codeArraySize; codeIndex++)
+	for (int codeIndex = varArrayIterator; codeIndex < codeArraySize; codeIndex++) // loop through entire code array
 	{
 		if (code[codeIndex] == '=') // found a variable declaration
 		{
 			for (int variableIndex = codeIndex; variableIndex > 0; variableIndex--) // start from '='
 			{
-				if (code[variableIndex] == 't' && code[variableIndex - 1] == 'n' && code[variableIndex - 2] == 'i' && code[variableIndex + 6] != '(') // find 'int' but not 'int main'
+				if (code[variableIndex] == 't' 
+					&& code[variableIndex - 1] == 'n' 
+					&& code[variableIndex - 2] == 'i' 
+					&& code[variableIndex + 6] != '(') // find 'int' but not 'int main'
 				{
 					for (int variableNameIndex = variableIndex + 2; code[variableNameIndex] != ' '; variableNameIndex++) // start from variable name, end at next space
 					{
 						variables[varArrayIterator] = code[variableNameIndex]; // assign each character in variable name to the new array, starting at element[0]
-						varArrayIterator++; // increment the iterator
+						varArrayIterator++; 
 					}
+					variables[varArrayIterator++] = ','; // comma separator between variables
+					break;
+				}
+				else if (code[variableIndex] == 'r'
+					&& code[variableIndex - 1] == 'a'
+					&& code[variableIndex - 2] == 'h'
+					&& code[variableIndex - 3] == 'c') // find 'char'
+				{
+					for (int variableNameIndex = variableIndex + 2; code[variableNameIndex] != ' '; variableNameIndex++) // start from variable name, end at next space
+					{
+						variables[varArrayIterator] = code[variableNameIndex]; // assign each character in variable name to the new array, starting at element[0]
+						varArrayIterator++; 
+					}
+					variables[varArrayIterator++] = ','; // comma separator between variables
+					break;
 				}
 			}
 		}
 	}
 
-	for (int i = 0; i < codeArraySize; i++)
+	if (varArrayIterator > 0) // if variablesArray is not empty
 	{
-		std::cout << variables[i];
+		variables[varArrayIterator - 1] = '\0'; // null-terminate the array of characters - now a string
 	}
+
+	std::cout << variables << std::endl; // variables array is now null-terminated, so it can be printed like a string data type
 }
 void FindAllVariablesInScope(char variables[1000], char code[1000], int lineNumber)
 {

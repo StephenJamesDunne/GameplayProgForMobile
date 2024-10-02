0475683fe695767419979663febc65d368a56e76
Stephen Dunne <C00194821@setu.ie>
2024-10-02 14:30:01 +0100

    lab 3 done

--- Lab3/Lab3/lab3.cpp
+++ Lab3/Lab3/lab3.cpp
@@ -8,14 +8,14 @@
 int main()
 {
 
-	/*char code[1000] = "int main() \n { \n int num1 = 5;\n int num2 = 7;\n char CharacterCharIsACharacterTest = 'a';\n if (num1 < 10) \n  {\n int num3 = 4;\n} \n} \n";
+	/*char code[1000] = "int returnTest() \n { \n } \n \n int main() \n { \n int num1 = 5;\n int num2 = 7;\n char CharacterCharIsACharacterTest = 'a';\n if (num1 < 10) \n  {\n int num3 = 4;\n} \n} \n";
 	char variables[1000] = "";
 	FindAllVariables(variables, code);*/
 
 
 	char code[1000] = "int main()\n{\n int numElephants = 10; \n int var2 = 10; \n	if (var2 < 10)\n{\nint var3 = 20; \n}\n while (var2 < 0)\n {\n if (var2 == 10) \n {\n int var4; \n} \n	int num = 10; \n} \n int var5 = 10; \n}";
 	char variables[1000] = "";
-	FindAllVariablesInScope(variables, code, 12);
+	FindAllVariablesInScope(variables, code, 14);
 
 	std::cin.get();
 }
--- Lab3/Lab3/lab3.h
+++ Lab3/Lab3/lab3.h
@@ -18,13 +18,14 @@ void FindAllVariables(char variables[1000], char code[1000])
 			{
 				if (code[variableIndex] == 't' 
 					&& code[variableIndex - 1] == 'n' 
-					&& code[variableIndex - 2] == 'i' 
-					&& code[variableIndex + 6] != '(') // find 'int' but not 'int main'
+					&& code[variableIndex - 2] == 'i') 
 				{
-					for (int variableNameIndex = variableIndex + 2; code[variableNameIndex] != ' '; variableNameIndex++) // start from variable name, end at next space
+					for (int variableNameIndex = variableIndex + 2; code[variableNameIndex] != ' ' 
+						&& code[variableNameIndex] != '='
+						&& code[variableNameIndex] != ';'; variableNameIndex++) // start from variable name, end at next space
 					{
 						variables[varArrayIterator] = code[variableNameIndex]; // assign each character in variable name to the new array, starting at element[0]
-						varArrayIterator++; 
+						varArrayIterator++;
 					}
 					variables[varArrayIterator++] = ','; // comma separator between variables
 					break;
@@ -34,7 +35,9 @@ void FindAllVariables(char variables[1000], char code[1000])
 					&& code[variableIndex - 2] == 'h'
 					&& code[variableIndex - 3] == 'c') // find 'char'
 				{
-					for (int variableNameIndex = variableIndex + 2; code[variableNameIndex] != ' '; variableNameIndex++) // start from variable name, end at next space
+					for (int variableNameIndex = variableIndex + 2; code[variableNameIndex] != ' '
+						&& code[variableNameIndex] != '='
+						&& code[variableNameIndex] != ';'; variableNameIndex++) // start from variable name, end at next space
 					{
 						variables[varArrayIterator] = code[variableNameIndex]; // assign each character in variable name to the new array, starting at element[0]
 						varArrayIterator++; 
@@ -55,5 +58,78 @@ void FindAllVariables(char variables[1000], char code[1000])
 }
 void FindAllVariablesInScope(char variables[1000], char code[1000], int lineNumber)
 {
+	int codeArraySize = 1000;
+	int noOfNewlines = 0;
+	int lineToStart = 0;
+	int varArrayIterator = 0;
 
+
+	// find element in the code array corresponding to the lineNumber variable
+	for (int codeIndex = 0; codeIndex < codeArraySize; codeIndex++) 
+	{
+		if (code[codeIndex] == '\n')
+		{
+			noOfNewlines++;
+		}
+
+		if (noOfNewlines == lineNumber)
+		{
+			lineToStart = codeIndex; // the lineNumber argument is now the end of the range for a loop
+			break;
+		}
+	}
+
+	for (lineToStart; lineToStart >= 0; lineToStart--) // start from lineNumber, count backwards through the code array up until the beginning of array
+	{
+		// found new scope block
+		if (code[lineToStart] == '}') 
+		{
+			for (int inScope = lineToStart; inScope >= 0; inScope--)
+			{
+				if (code[inScope] == '{')
+				{
+					lineToStart = inScope; // start at this new line before storing variables in new array
+					break;
+				}
+			}
+		}
+		else
+		{
+			if (code[lineToStart] == 't'
+				&& code[lineToStart - 1] == 'n'
+				&& code[lineToStart - 2] == 'i'
+				&& code[lineToStart + 6] != '(')
+			{
+				for (int variableNameIndex = lineToStart + 2; code[variableNameIndex] != ' '
+					&& code[variableNameIndex] != '='
+					&& code[variableNameIndex] != ';'; variableNameIndex++) // start from variable name, end at next space
+				{
+					variables[varArrayIterator] = code[variableNameIndex]; // assign each character in variable name to the new array, starting at element[0]
+					varArrayIterator++;
+				}
+				variables[varArrayIterator++] = ','; // comma separator between variables
+			}
+			else if (code[lineToStart] == 'r'
+				&& code[lineToStart - 1] == 'a'
+				&& code[lineToStart - 2] == 'h'
+				&& code[lineToStart - 3] == 'c') // find 'char'
+			{
+				for (int variableNameIndex = lineToStart + 2; code[variableNameIndex] != ' '
+					&& code[variableNameIndex] != '='
+					&& code[variableNameIndex] != ';'; variableNameIndex++) // start from variable name, end at next space
+				{
+					variables[varArrayIterator] = code[variableNameIndex]; // assign each character in variable name to the new array, starting at element[0]
+					varArrayIterator++;
+				}
+				variables[varArrayIterator++] = ','; // comma separator between variables
+			}
+		}
+	}
+
+	if (varArrayIterator > 0) // if variablesArray is not empty
+	{
+		variables[varArrayIterator - 1] = '\0'; // null-terminate the array of characters - now a string
+	}
+
+	std::cout << variables << std::endl;
 }
